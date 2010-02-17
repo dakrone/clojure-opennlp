fcf326944952fe44e0ad9f9a85f4a7f91bd9062b
dakrone <lee@writequit.org>
2010-02-16 19:38:51 -0700

    adding opennlp jars to the lib and a leiningen project file.

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject org.clojars.thnetos/opennlp "0.0.1"
+  :description "Natural Language Processing with Clojure, library for opennlp"
+  :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]]
+  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
+                     [org.clojure/clojure-contrib "1.0-SNAPSHOT"]])
+
