795daa17a480eae6a38ddb7ac30913fe174a7f0b
dakrone <lee@writequit.org>
2010-03-01 22:41:35 -0700

    update project.clj with github site in the description so clojars actual has a link to some kind of documentation

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject org.clojars.thnetos/opennlp "0.0.1"
-  :description "Natural Language Processing with Clojure, library for opennlp"
+  :description "Natural Language Processing with Clojure, library for opennlp. http://github.com/dakrone/clojure-opennlp"
   :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
                      [org.clojure/clojure-contrib "1.0-SNAPSHOT"]])
