7522021dee7c964d334150ca952d7391482ee334
dakrone <lee@writequit.org>
2010-03-09 13:03:12 -0700

    Removing jars from lib folder (they aren't necessary any more). Updated dependencies in project.clj so 'lein deps' should work now. Pushed to version 0.0.2

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject org.clojars.thnetos/opennlp "0.0.1"
+(defproject org.clojars.thnetos/opennlp "0.0.2"
   :description "Natural Language Processing with Clojure, library for opennlp. http://github.com/dakrone/clojure-opennlp"
-  :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]]
-  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
-                     [org.clojure/clojure-contrib "1.0-SNAPSHOT"]])
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
+                 [org.clojars.thnetos/opennlp-tools "1.4.3"]]
+  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]])
 
