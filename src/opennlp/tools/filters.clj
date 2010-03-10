87efce3dd35db574d463e4d94f8a5fe9d8276eb7
dakrone <lee@writequit.org>
2010-03-10 15:50:41 -0700

    Added chunk-filter filter generator for treebank-chunked sentences. Added a few example filters for noun/verb phrases as well as adjective/adverb phrases.

--- src/opennlp/tools/filters.clj
+++ src/opennlp/tools/filters.clj
@@ -10,9 +10,23 @@
        (filter (fn [t#] (re-find ~r (second t#))) elements#))))
 
 
+(defmacro chunk-filter
+  "Declare a filter for treebank-chunked lists with the given name and regex."
+  [n r]
+  (let [docstring (str "Given a list of treebank-chunked elements, return only the " n " in a list.")]
+    `(defn ~n
+       ~docstring
+       [elements#]
+       (filter (fn [t#] (re-find ~r (first t#))) elements#))))
+
+
 ; It's easy to define your own filters!
 (pos-filter nouns #"^NN")
 (pos-filter nouns-and-verbs #"^(NN|VB)")
 (pos-filter proper-nouns #"^NNP")
 (pos-filter verbs #"^VB")
 
+(chunk-filter verb-phrases #"^VP$")
+(chunk-filter noun-phrases #"^NP$")
+(chunk-filter adverb-phrases #"^ADVP$")
+(chunk-filter adjective-phrases #"^ADJP$")
