6675cd94170a6a320dcb2cf7d4aef071f151e536
dakrone <lee@writequit.org>
2010-03-10 13:11:01 -0700

    Added function identifiers for all the make-* functions. Added the beginnings of a treebank-chunker. It works, but I'm not extirely set on the output format.

--- src/opennlp/nlp.clj
+++ src/opennlp/nlp.clj
@@ -9,6 +9,7 @@
   (:import [opennlp.tools.tokenize TokenizerME])
   (:import [opennlp.tools.sentdetect SentenceDetectorME])
   (:import [opennlp.tools.namefind NameFinderME])
+  (:import [opennlp.tools.chunker ChunkerME])
   (:import [opennlp.tools.postag POSTaggerME DefaultPOSContextGenerator POSContextGenerator]))
 
 ; OpenNLP property for pos-tagging
@@ -24,7 +25,7 @@
   [modelfile]
   (if (not (file-exist? modelfile))
     (throw (FileNotFoundException. "Model file does not exist."))
-    (fn
+    (fn sentenizer
       [text]
       (let [model     (.getModel (SuffixSensitiveGISModelReader. (File. modelfile)))
             detector  (SentenceDetectorME. model)
@@ -37,7 +38,7 @@
   [modelfile]
   (if (not (file-exist? modelfile))
     (throw (FileNotFoundException. "Model file does not exist."))
-    (fn
+    (fn tokenizer
       [sentence]
       (let [model     (.getModel (SuffixSensitiveGISModelReader. (File. modelfile)))
             tokenizer (TokenizerME. model)
@@ -50,7 +51,7 @@
   [modelfile]
   (if (not (file-exist? modelfile))
     (throw (FileNotFoundException. "Model file does not exist."))
-    (fn
+    (fn pos-tagger
       [tokens]
       (let [#^POSContextGenerator cg (DefaultPOSContextGenerator. nil)
             model  (.getModel (SuffixSensitiveGISModelReader. (File. modelfile)))
@@ -64,7 +65,7 @@
   [& modelfiles]
   (if (not (reduce 'and (map file-exist? modelfiles)))
     (throw (FileNotFoundException. "Not all model files exist."))
-    (fn
+    (fn name-finder
       [tokens]
       (distinct
         (flatten
@@ -73,6 +74,76 @@
                   finder  (NameFinderME. model)
                   matches (.find finder tokens)]
               (map #(nth tokens (.getStart %)) matches))))))))
+
+
+(defn- split-chunks
+  "Partition a sequence of treebank chunks by their phrases."
+  [chunks]
+  (let [seqnum    (atom 0)
+        splitfunc (fn
+                    [item]
+                    (if (.startsWith item "B-")
+                      (swap! seqnum inc)
+                      @seqnum))]
+    (partition-by splitfunc (pop chunks))))
+
+
+(defn- size-chunk
+  "Given a chunk ('B-NP' 'I-NP' 'I-NP' ...), return a vector of the
+  chunk type and item count.
+
+  So, for ['B-NP' 'I-NP' 'I-NP' 'B-VP' 'B-VP' 'I-VP'] it would return:
+  ['B-NP' 3] ['B-VP' 1] ['B-VP' 2]."
+  [tb-chunk]
+  (let [chunk-type  (second (re-find #"B-(.*)" (first tb-chunk)))
+        chunk-count (count tb-chunk)]
+    [chunk-type chunk-count]))
+
+
+; Thanks chouser
+(defn- split-with-size
+  [[v & vs] s]
+  (if-not v
+    (list s)
+    (cons (take v s) (split-at* vs (drop v s)))))
+
+
+(defn make-treebank-chunker
+  "Return a function for chunking phrases from pos-tagged tokens based on
+  a given model file."
+  [modelfile]
+  (if (not (file-exist? modelfile))
+    (throw (FileNotFoundException. "Model file does not exist."))
+    (fn treebank-chunker
+      [pos-tagged-tokens]
+      (let [model        (.getModel (SuffixSensitiveGISModelReader. (File. modelfile)))
+            chunker      (ChunkerME. model)
+            tokens       (map first pos-tagged-tokens)
+            tags         (map second pos-tagged-tokens)
+            chunks       (into [] (seq (.chunk chunker tokens tags)))
+            sized-chunks (map size-chunk (split-chunks chunks))
+            types        (map first sized-chunks)
+            sizes        (map last sized-chunks)
+            token-chunks (split-with-size sizes tokens)]
+        (partition 2 (interleave types token-chunks))))))
+
+
+; Docs for treebank chunking:
+
+;(def chunker (make-treebank-chunker "models/EnglishChunk.bin.gz"))
+;(pprint (chunker (pos-tag (tokenize "The override system is meant to deactivate the accelerator when the brake pedal is pressed."))))
+
+;(map size-chunk (split-chunks (chunker (pos-tag (tokenize "The override system is meant to deactivate the accelerator when the brake pedal is pressed.")))))
+
+;opennlp.nlp=> (split-with-size (sizes (map size-chunk (split-chunks (chunker (pos-tag (tokenize "The override system is meant to deactivate the accelerator when the brake pedal is pressed.")))))) (tokenize "The override system is meant to deactivate the accelerator when the brake pedal is pressed."))
+;(("The" "override" "system") ("is" "meant" "to" "deactivate") ("the" "accelerator") ("when") ("the" "brake" "pedal") ("is" "pressed") ("."))
+
+;(["NP" 3] ["VP" 4] ["NP" 2] ["ADVP" 1] ["NP" 3] ["VP" 2])
+
+;opennlp.nlp=> (pprint (chunker (pos-tag (tokenize "The override system is meant to deactivate the accelerator when the brake pedal is pressed."))))
+;#<ArrayList [B-NP, I-NP, I-NP, B-VP, I-VP, I-VP, I-VP, B-NP, I-NP, B-ADVP, B-NP, I-NP, I-NP, B-VP, I-VP, O]>
+
+; So, B-* starts a sequence, I-* continues it. New phrase starts when B-* is encountered
 
 
 
@@ -89,6 +160,7 @@
 (def tokenize (make-tokenizer "models/EnglishTok.bin.gz"))
 (def pos-tag (make-pos-tagger "models/tag.bin.gz"))
 (def name-find (make-name-finder "models/namefind/person.bin.gz" "models/namefind/organization.bin.gz"))
+(def chunker (make-treebank-chunker "models/EnglishChunk.bin.gz"))
 
 (pprint (get-sentences "First sentence. Second sentence? Here is another one. And so on and so forth - you get the idea..."))
 
