37c134559e9c7993bc42ec709aa1d7c2dc86e732
dakrone <lee@writequit.org>
2010-08-11 11:01:41 -0600

    Bump back down to clojure 1.1, there's no need for 1.2... yet.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject org.clojars.thnetos/opennlp "0.0.6"
   :description "Natural Language Processing with Clojure, library for opennlp. http://github.com/dakrone/clojure-opennlp"
-  :dependencies [[org.clojure/clojure "1.2.0-RC1"]
-                 [org.clojure/clojure-contrib "1.2.0-RC1"]
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.1.0"]
                  [org.clojars.thnetos/opennlp-tools "1.4.3"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]])
 
