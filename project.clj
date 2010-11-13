5a57f18f0815c237a11dd37e48b9ebb53826006c
dakrone <lee@writequit.org>
2010-11-13 15:52:21 -0700

    remove java packaging name from the project name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.thnetos/clojure-opennlp "0.1.0"
+(defproject clojure-opennlp "0.1.0"
   :description "Natural Language Processing with Clojure, library for opennlp. http://github.com/dakrone/clojure-opennlp"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
