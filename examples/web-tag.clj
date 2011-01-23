8ff3bfd6ed23e2de5aab525436331e7b1b165619
Lee Hinman <lee@writequit.org>
2011-01-22 18:32:04 -0700

    Fix web-tag.clj example for clojure and clojure-opennlp updates

--- examples/web-tag.clj
+++ examples/web-tag.clj
@@ -1,8 +1,6 @@
-(ns web-tag
+(ns examples.web-tag
   (:use [opennlp.nlp]) ; http://github.com/dakrone/clojure-opennlp
-  (:use [clojure.contrib.pprint])
-  (:use [clojure.contrib.duck-streams :only [slurp*]])
-  (:use [clojure.contrib.seq-utils :only [partition-all]]))
+  (:use [clojure.pprint :only [pprint]]))
 
 (defn strip-html-tags
   "Messily strip html tags from a web page"
@@ -17,16 +15,16 @@
 
 (defn fetch-page
   [url]
-  (let [html (.replaceAll (slurp* url) "[\t\n\r]" " ")]
+  (let [html (.replaceAll (slurp url) "[\t\n\r]" " ")]
     (re-find #"<body.*?</body>" html)))
 
 (defn fetch-plain-page
   [url]
   (strip-html-tags (fetch-page url)))
 
-(def get-sentences (make-sentence-detector "models/EnglishSD.bin.gz"))
-(def tokenize (make-tokenizer "models/EnglishTok.bin.gz"))
-(def pos-tag (make-pos-tagger "models/tag.bin.gz"))
+(def get-sentences (make-sentence-detector "models/en-sent.bin"))
+(def tokenize (make-tokenizer "models/en-token.bin"))
+(def pos-tag (make-pos-tagger "models/en-pos-maxent.bin"))
 
 (defn- tag-sentences
   [sent-seq]
