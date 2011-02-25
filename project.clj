295968218ab61b6af5572c1d6d24276710964753
Lee Hinman <lee@writequit.org>
2011-02-24 22:19:53 -0700

    update marginalia

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [opennlp/tools "1.5.0"]]
-  :dev-dependencies [[marginalia "0.3.2"]]
+  :dev-dependencies [[marginalia "0.5.0"]]
   :jvm-opts ["-Xmx1024m"]
   :repositories {"opennlp.sf.net" "http://opennlp.sourceforge.net/maven2"})
 
