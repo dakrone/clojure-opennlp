21ad3a0fffa804b0d9f4a12e877eef695a1c451f
Lee Hinman <lee@writequit.org>
2011-03-17 11:40:20 -0600

    string cleanup

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject clojure-opennlp "0.1.4-SNAPSHOT"
-  :description "Natural Language Processing with Clojure, library for opennlp. http://github.com/dakrone/clojure-opennlp"
+  :description "Natural Language Processing with Clojure, library for opennlp.
+                http://github.com/dakrone/clojure-opennlp"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [opennlp/tools "1.5.0"]]
