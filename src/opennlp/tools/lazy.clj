e89318c60774144ba16ecf14dbca9ff1c3454594
Lee Hinman <lee@writequit.org>
2011-03-17 11:22:18 -0600

    whitespace fixes

--- src/opennlp/tools/lazy.clj
+++ src/opennlp/tools/lazy.clj
@@ -1,15 +1,15 @@
 (ns opennlp.tools.lazy
   "Tools for lazily separating, tokenizing and tagging sentences.")
 
-
+;; TODO: collapse these 3 functions into a generic one
 (defn lazy-get-sentences
   "Given a sequence of texts and a sentence-finder, return a lazy sequence of
   sentences for each text."
   [text sentence-finder]
   (lazy-seq
-    (when-let [s (seq text)]
-      (cons (sentence-finder (first text))
-            (lazy-get-sentences (rest s) sentence-finder)))))
+   (when-let [s (seq text)]
+     (cons (sentence-finder (first text))
+           (lazy-get-sentences (rest s) sentence-finder)))))
 
 
 (defn lazy-tokenize
@@ -17,8 +17,8 @@
   tokenized sentences."
   [sentences tokenizer]
   (lazy-seq
-    (when-let [s (seq sentences)]
-      (cons (tokenizer (first s)) (lazy-tokenize (rest s) tokenizer)))))
+   (when-let [s (seq sentences)]
+     (cons (tokenizer (first s)) (lazy-tokenize (rest s) tokenizer)))))
 
 
 (defn lazy-tag
@@ -26,9 +26,9 @@
   sequence of pos-tagged sentences."
   [sentences tokenizer pos-tagger]
   (lazy-seq
-    (when-let [s (seq sentences)]
-      (cons (pos-tagger (tokenizer (first s)))
-            (lazy-tag (rest s) tokenizer pos-tagger)))))
+   (when-let [s (seq sentences)]
+     (cons (pos-tagger (tokenizer (first s)))
+           (lazy-tag (rest s) tokenizer pos-tagger)))))
 
 
 (defn lazy-chunk
@@ -36,8 +36,7 @@
   return a lazy sequence of treebank-chunked sentences."
   [sentences tokenizer pos-tagger chunker]
   (lazy-seq
-    (when-let [s (seq sentences)]
-      (cons (chunker (pos-tagger (tokenizer (first s))))
-            (lazy-chunk (rest s) tokenizer pos-tagger chunker)))))
+   (when-let [s (seq sentences)]
+     (cons (chunker (pos-tagger (tokenizer (first s))))
+           (lazy-chunk (rest s) tokenizer pos-tagger chunker)))))
 
-; TODO: collapse these 3 functions into a generic one
