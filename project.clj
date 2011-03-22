350ad22a186ce35bf78d5a7b9b58f0f9b563d03a
Lee Hinman <lee@writequit.org>
2011-03-22 16:22:15 -0600

    move to clojure 1.2.1

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject clojure-opennlp "0.1.6-SNAPSHOT"
   :description "Natural Language Processing with Clojure, library for opennlp.
                 http://github.com/dakrone/clojure-opennlp"
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [opennlp/tools "1.5.0"]]
   :dev-dependencies [[marginalia "0.5.0"]]
