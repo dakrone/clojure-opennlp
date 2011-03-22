f8dae01ee95274ba9e4c9c87f2f0963d0d78e5e8
Lee Hinman <lee@writequit.org>
2011-03-21 22:13:39 -0600

    bump to next snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-opennlp "0.1.5"
+(defproject clojure-opennlp "0.1.6-SNAPSHOT"
   :description "Natural Language Processing with Clojure, library for opennlp.
                 http://github.com/dakrone/clojure-opennlp"
   :dependencies [[org.clojure/clojure "1.2.0"]
