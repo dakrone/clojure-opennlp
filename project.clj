ac875033fc27b9b959bbb185eb6211d942e02bf3
Lee Hinman <lee@writequit.org>
2011-04-26 21:47:40 -0600

    add slamhound

--- project.clj
+++ project.clj
@@ -3,8 +3,10 @@
                 http://github.com/dakrone/clojure-opennlp"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [opennlp/tools "1.5.0"]]
-  :dev-dependencies [[marginalia "0.5.0"]]
+                 [opennlp/tools "1.5.0"]
+                 [slamhound "1.1.1"]]
+  :dev-dependencies [[marginalia "0.5.0"]
+                     [slamhound "1.1.1"]]
   :jvm-opts ["-Xmx1024m"]
   :repositories {"opennlp.sf.net" "http://opennlp.sourceforge.net/maven2"})
 
