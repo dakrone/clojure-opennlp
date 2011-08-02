4e314fc7b564fcd94e9e492142d495193b4c77ec
Lee Hinman <lee@writequit.org>
2011-08-01 23:20:35 -0600

    add marginalia to deps

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.apache.opennlp/opennlp-tools "1.5.1-incubating"]]
   :dev-dependencies [[slamhound "1.1.1"]
+                     [lein-marginalia "0.6.0"]
                      [lein-multi "1.0.0"]]
   :multi-deps {"1.3" [[org.clojure/clojure "1.3.0-beta1"]
                       [org.apache.opennlp/opennlp-tools "1.5.1-incubating"]]}
