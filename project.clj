2760ba6bfa4d251a6cf07329cf371857b26cc168
Lee Hinman <lee@writequit.org>
2011-09-23 15:55:32 -0600

    bump clojure version to 1.3, multi deps to 1.2.1

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject clojure-opennlp "0.1.8-SNAPSHOT"
   :description "Natural Language Processing with Clojure, library for opennlp.
                 http://github.com/dakrone/clojure-opennlp"
-  :dependencies [[org.clojure/clojure "1.2.1"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.apache.opennlp/opennlp-tools "1.5.1-incubating"]]
   :dev-dependencies [[slamhound "1.1.1"]
                      [lein-marginalia "0.6.0"]
                      [lein-multi "1.0.0"]]
-  :multi-deps {"1.3" [[org.clojure/clojure "1.3.0-RC0"]
+  :multi-deps {"1.2.1" [[org.clojure/clojure "1.2.1"]
                       [org.apache.opennlp/opennlp-tools "1.5.1-incubating"]]}
   :jvm-opts ["-Xmx1024m"])
 
