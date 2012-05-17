f44de9ccdc22bdb55cfb0671fb093966d36c22a1
Lee Hinman <lee@writequit.org>
2012-05-17 08:55:09 -0600

    switch to leiningen 2

--- project.clj
+++ project.clj
@@ -1,14 +1,11 @@
 (defproject clojure-opennlp "0.1.10-SNAPSHOT"
   :description "Natural Language Processing with Clojure, library for opennlp."
   :url "http://github.com/dakrone/clojure-opennlp"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.apache.opennlp/opennlp-tools "1.5.1-incubating"]]
-  :dev-dependencies [[slamhound "1.1.1"]
-                     [lein-marginalia "0.6.0"]
-                     [lein-multi "1.0.0"]]
-  :multi-deps {"1.2.1" [[org.clojure/clojure "1.2.1"]
-                        [org.apache.opennlp/opennlp-tools "1.5.1-incubating"]]
-               "1.4.0" [[org.clojure/clojure "1.4.0-alpha1"]
-                        [org.apache.opennlp/opennlp-tools "1.5.1-incubating"]]}
+  :profiles {:1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}
+             :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}}
+  :aliases {"all" ["with-profile" "dev,1.2:dev,1.3:dev"]}
   :jvm-opts ["-Xmx2048m"])
 
