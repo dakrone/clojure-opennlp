4a13b99d9cd3623e9632683aa76f5f2fcb662f1d
Lee Hinman <lee@writequit.org>
2012-10-16 10:02:00 -0600

    add 1.5 as a multi dep

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.apache.opennlp/opennlp-tools "1.5.2-incubating"]]
   :profiles {:1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}
-             :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}}
-  :aliases {"all" ["with-profile" "dev,1.2:dev,1.3:dev"]}
+             :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.0-alpha6"]]}}
+  :aliases {"all" ["with-profile" "dev,1.2:dev,1.3:dev:dev,1.5"]}
   :jvm-opts ["-Xmx2048m"])
 
