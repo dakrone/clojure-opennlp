2867af59c8cfaa4449da15cd17b50932c7d7e42c
Lee Hinman <lee@writequit.org>
2013-03-14 18:51:04 -0600

    Bump clojure version, stop supporting 1.2.x

--- project.clj
+++ project.clj
@@ -2,11 +2,10 @@
   :description "Natural Language Processing with Clojure, library for opennlp."
   :url "http://github.com/dakrone/clojure-opennlp"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.apache.opennlp/opennlp-tools "1.5.2-incubating"]]
-  :profiles {:1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}
-             :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.0-alpha6"]]}}
-  :aliases {"all" ["with-profile" "dev,1.2:dev,1.3:dev:dev,1.5"]}
+  :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}}
+  :aliases {"all" ["with-profile" "dev,1.3:dev,1.4:dev"]}
   :jvm-opts ["-Xmx2048m"])
 
