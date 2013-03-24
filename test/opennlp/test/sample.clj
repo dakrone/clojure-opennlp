0fa20eee0e23da39ec12698f97471149edc80c12
Kevin Downey <redchin@gmail.com>
2013-03-24 14:39:26 -0700

    don't explode when tested with clojure 1.3

--- test/opennlp/test/sample.clj
+++ test/opennlp/test/sample.clj
@@ -1,14 +1,21 @@
 (ns opennlp.test.sample
-  (:require [clojure.test :refer :all]
-            [opennlp.sample :refer [clojure-document-sample-stream]])
+  (:use [clojure.test]
+        [opennlp.sample])
   (:import (opennlp.tools.doccat DocumentSample)))
 
+(use-fixtures :once
+              (fn [f]
+                (when (and (>= (:major *clojure-version*) 1)
+                           (or (> (:major *clojure-version*) 1)
+                               (> (:minor *clojure-version*) 3)))
+                  (f))))
+
 (deftest test-samples-round-trip
-  (let [d #opennlp/sample {:category "foo" :text ["bar"]}]
+  (let [d (DocumentSample. "foo" "bar")]
     (is (= d (read-string (pr-str d))))))
 
 (deftest test-clojure-document-sample-stream
-  (let [d #opennlp/sample {:category "foo" :text ["bar"]}
+  (let [d (DocumentSample. "foo" "bar")
         x (java.io.ByteArrayInputStream.
            (.getBytes
             (with-out-str
