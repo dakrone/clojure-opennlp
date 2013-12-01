c36090afa8c0dc6ad1777a890a06e7cb32f58531
Lee Hinman <lee@writequit.org>
2013-12-01 15:50:50 -0700

    Bump instaparse to 1.2.12, fixing #32

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/dakrone/clojure-opennlp"
   :min-lein-version "2.0.0"
   :dependencies [[org.apache.opennlp/opennlp-tools "1.5.3"]
-                 [instaparse "1.0.1"]]
+                 [instaparse "1.2.12"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}}
   :aliases {"all" ["with-profile" "dev,1.4:dev"]}
