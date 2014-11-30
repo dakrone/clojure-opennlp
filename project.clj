aa3db4466d31dfc8bd216e8209d3d4cd9653aeef
Lee Hinman <lee@writequit.org>
2014-11-30 23:34:42 +0100

    Bump dependencies

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.apache.opennlp/opennlp-tools "1.5.3"]
-                 [instaparse "1.2.12"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}}
-  :aliases {"all" ["with-profile" "dev,1.4:dev"]}
+                 [instaparse "1.3.4"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}}
+  :aliases {"all" ["with-profile" "dev,1.5:dev"]}
   :jvm-opts ["-Xmx2048m"])
