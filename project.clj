e3562f5e49394a2b2cb6cfffde09dcf6f1e676fc
Ruediger Gad <r.c.g@gmx.de>
2016-04-23 15:01:28 +0200

    Update project.clj
    - Use final Clojure 1.7.0 version.
    - Add Clojure version 1.8.0.
    - Update instaparse to 1.4.1.

--- project.clj
+++ project.clj
@@ -5,10 +5,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.apache.opennlp/opennlp-tools "1.5.3"]
-                 [instaparse "1.4.0"]]
+                 [instaparse "1.4.1"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]]
                    :plugins [[lein-marginalia "0.8.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha6"]]}}
-  :aliases {"all" ["with-profile" "dev,1.5:dev:dev,1.7"]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
+             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}}
+  :aliases {"all" ["with-profile" "dev,1.5:dev:dev,1.7:dev,1.8"]}
   :jvm-opts ["-Xmx2048m"])
