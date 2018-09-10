a1a1e25da0c07a5ead0ca8f0d01b94af54c7a98c
Lee Hinman <lee@writequit.org>
2018-09-10 17:05:39 -0600

    Bump to 0.5.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-opennlp "0.4.1-SNAPSHOT"
+(defproject clojure-opennlp "0.5.0-SNAPSHOT"
   :description "Natural Language Processing with Clojure, library for opennlp."
   :url "http://github.com/dakrone/clojure-opennlp"
   :min-lein-version "2.0.0"
@@ -6,11 +6,11 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.apache.opennlp/opennlp-tools "1.9.0"]
                  [instaparse "1.4.9"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]]
                    :plugins [[lein-marginalia "0.8.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
-             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
-             :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}}
-  :aliases {"all" ["with-profile" "dev,1.5:dev:dev,1.7:dev,1.8:dev,1.9"]}
+             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}}
+  :aliases {"all" ["with-profile" "dev,1.5:dev,1.6:dev,1.7:dev,1.8:dev"]}
   :jvm-opts ["-Xmx2048m"])
